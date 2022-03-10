6ab27ff9a7d746a69e515063b8a968321ff4999c
MacMillan-dev <munenenjue18@gmail.com>
2022-03-10 13:26:06 -0800

    command that prints size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
   long long int la;
   float f;
 
-  printf("Size of a char: %ld byte(s)\n", sizeof(c));
-  printf("Size of an int: %ld byte(s)\n", sizeof(i));
-  printf("Size of a long int: %ld byte(s)\n", sizeof(l));
-  printf("Size of a long long int: %ld byte(s)\n", sizeof(la));
-  printf("Size of a float: %ld byte(s)\n", sizeof(f));
+  printf("Size of a char: %ld byte(s)", sizeof(c));
+  printf("Size of an int: %ld byte(s)", sizeof(i));
+  printf("Size of a long int: %ld byte(s)", sizeof(l));
+  printf("Size of a long long int: %ld byte(s)", sizeof(la));
+  printf("Size of a float: %ld byte(s)", sizeof(f));
   return (0);
 }
