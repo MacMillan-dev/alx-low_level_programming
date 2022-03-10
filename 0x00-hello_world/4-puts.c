713467e45c1a831d856ae939bad2c5c950a9b11c
MacMillan-dev <munenenjue18@gmail.com>
2022-03-10 12:28:59 -0800

    script that prints string followed by newline

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 
 /**
  *main - prints out the string in the puts function
- *Description: This program prints "Programming is like building a multilingual puzzle" and returns 0 using the main function
+ *Description: This program prints 
+ *"Programming is like building a multilingual puzzle"
+ *and returns 0 using the main function
  *Return: 0
  */
 
-int main (void)
+int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
+    puts ("\"Programming is like building a multilingual puzzle");
     return(0);
 }
