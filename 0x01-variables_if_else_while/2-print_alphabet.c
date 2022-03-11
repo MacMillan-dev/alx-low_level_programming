e24ded520424a7d36c97f2babffd728911865c9b
MacMillan-dev <munenenjue18@gmail.com>
2022-03-11 11:38:13 -0800

    program that prints alphabets and a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - returns the alphabet in lowercase
+ *followed by a new line
+ *return: Always 0(success)
+ */
+int main(void)
+{
+        putchar ch = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z\n";
+	putchar (ch);
+	return (0);
+}
