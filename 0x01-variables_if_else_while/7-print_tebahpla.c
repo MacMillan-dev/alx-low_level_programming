b1c043832961962b4e4c523612e5ba3b8e26c904
MacMillan-dev <munenenjue18@gmail.com>
2022-03-13 07:30:33 -0700

    prints lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *
+ *return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+ch = 'z'
+do {putchar(ch);
+ch--;
+} while (ch >= 'a');
+putchar('\n');
+return (0);
+}
