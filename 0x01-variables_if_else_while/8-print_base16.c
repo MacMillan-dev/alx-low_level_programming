be829c24ffb40eee91c97352c13346d72e015689
MacMillan-dev <munenenjue18@gmail.com>
2022-03-13 07:42:49 -0700

    print all numbers of base 16 in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *
+ *return: returns 0 (Success)
+ */
+int main(void)
+{
+int i;
+int b;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (b = 'a'; b <= 'f'; b++)
+{
+putchar(d);
+}
+putchar('\n');
+return (0);
+}
