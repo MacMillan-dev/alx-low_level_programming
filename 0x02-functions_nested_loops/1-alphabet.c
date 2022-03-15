f083d6811b6a995834dc63b5222c006136bb4ae5
MacMillan-dev <munenenjue18@gmail.com>
2022-03-14 23:15:55 -0700

    function that prints the alphabet in lowercase followed by a newline

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
