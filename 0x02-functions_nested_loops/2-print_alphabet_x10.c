0f6fc75fcdf078a221753ec0203f02792c825a36
MacMillan-dev <munenenjue18@gmail.com>
2022-03-15 16:50:02 -0700

    function that prints the alphabet in lowercase 10 times followed by \n

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
  /**
- *print_alphabet_x10 - Entry
+  *print_alphabet_x10 - Entry point
  *
  *Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 int i = 0;
-while (i < 10){ 
+while (i < 10)
+{
 char ch = 'a';
 while (ch <= 'z')
 {
@@ -15,6 +16,6 @@ _putchar(ch);
 ch++;
 }
 i++;
-}
 _putchar('\n');
+}
 }
