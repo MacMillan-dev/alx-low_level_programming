67e663598a21535dd6b8edcac84b250a1f434284
MacMillan-dev <munenenjue18@gmail.com>
2022-03-16 03:32:31 -0700

    program that prints string followed by new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
 #include "main.h"
  /**
- *main - Entry
+ *main - prints _putchar
  *
  *Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-char c = "_putchar";
-_putchar(c);
+char text[9] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(text[i]);
+}
 _putchar('\n');
 return (0);
 }
