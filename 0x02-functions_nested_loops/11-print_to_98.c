f8fdcc5d8f0cb09cf00f95132d74c41fb69b34e8
MacMillan-dev <munenenjue18@gmail.com>
2022-03-16 16:23:58 -0700

    prints all numbers from n to 98 followed by newline

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
- /**
-  *print_to_98 - prints all natural numbers from n to 98
-  *
-  *@n: integer param
-  */
+/**
+ *print_to_98 - prints all natural numbers from n to 98
+ *
+ *@n: integer param
+ */
 void print_to_98(int n)
 {
 if (n > 98)
