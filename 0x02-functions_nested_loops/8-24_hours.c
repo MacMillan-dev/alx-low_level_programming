bcec0ab7a2052cbb563e4af9f7a456417b63a6d4
MacMillan-dev <munenenjue18@gmail.com>
2022-03-16 15:39:43 -0700

    function that prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ for (min = 0; min < 60; min++)
 _putchar((hr / 10) + '0');
 _putchar((hr % 10) + '0');
 _putchar(':');
- _putchar((min / 10) + '0');
+_putchar((min / 10) + '0');
 _putchar((min % 10) + '0');
 _putchar('\n');
 }
